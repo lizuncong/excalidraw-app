05bf074df1a1d39299a5f7879211b8046582fc44
lizuncong <2900170896@qq.com>
2023-01-15 17:30:04 +0800

    feat: md

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -72,7 +72,8 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
-
+const lessRegex = /\.less$/;
+const lessModuleRegex = /\.module\.less$/;
 const hasJsxRuntime = (() => {
   if (process.env.DISABLE_NEW_JSX_TRANSFORM === "true") {
     return false;
@@ -508,8 +509,8 @@ module.exports = function (webpackEnv) {
             // By default we support SASS Modules with the
             // extensions .module.scss or .module.sass
             {
-              test: sassRegex,
-              exclude: sassModuleRegex,
+              test: lessRegex,
+              exclude: lessModuleRegex,
               use: getStyleLoaders(
                 {
                   importLoaders: 3,
@@ -520,7 +521,7 @@ module.exports = function (webpackEnv) {
                     mode: "icss",
                   },
                 },
-                "sass-loader"
+                "less-loader"
               ),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
@@ -531,7 +532,7 @@ module.exports = function (webpackEnv) {
             // Adds support for CSS Modules, but using SASS
             // using the extension .module.scss or .module.sass
             {
-              test: sassModuleRegex,
+              test: lessModuleRegex,
               use: getStyleLoaders(
                 {
                   importLoaders: 3,
@@ -543,7 +544,7 @@ module.exports = function (webpackEnv) {
                     getLocalIdent: getCSSModuleLocalIdent,
                   },
                 },
-                "sass-loader"
+                "less-loader"
               ),
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
--- src/pages/shapes/index.jsx
+++ src/pages/shapes/index.jsx
@@ -1,53 +1,9 @@
-import React, { useRef, useState, useEffect } from "react";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import rehypeRaw from "rehype-raw";
-import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
-import "github-markdown-css";
-
-import './index.less'
-
-
+import React from "react";
+import MarkDown from "@/components/markdown";
 import shape from "../../../doc/canvas图形绘制基础.md";
 
 function Shapes() {
-  const [mdText, setMdText] = useState("");
-  useEffect(() => {
-    fetch(shape)
-      .then((response) => response.text())
-      .then((text) => {
-        setMdText(text);
-      });
-  }, []);
-  return (
-    <div className="markdown-body">
-      <ReactMarkdown
-        children={mdText}
-        rehypePlugins={[rehypeRaw]}
-        remarkPlugins={[remarkGfm]}
-        // components={{
-        //   code({ node, inline, className, children, ...props }) {
-        //     console.log("clasname...", className);
-        //     const match = /language-(\w+)/.exec(className || "");
-        //     return !inline && match ? (
-        //       <SyntaxHighlighter
-        //         children={String(children).replace(/\n$/, "")}
-        //         style={tomorrow}
-        //         language={match[1]}
-        //         PreTag="div"
-        //         {...props}
-        //       />
-        //     ) : (
-        //       <code className={className} {...props}>
-        //         {children}
-        //       </code>
-        //     );
-        //   },
-        // }}
-      />
-    </div>
-  );
+  return <MarkDown src={shape} />;
 }
 
 export default Shapes;
