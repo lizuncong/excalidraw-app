0d1133a3c17e95f87b5bc50f002ef000ccbe2d7e
lizuncong <2900170896@qq.com>
2023-01-17 23:26:20 +0800

    feat: stash

--- src/pages/style/index.jsx
+++ src/pages/style/index.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 function Style() {
-  return <div>canvas样式和颜色</div>;
+  const canvasRef = useRef();
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    function draw() {
+      // ctx.lineWidth = 1;
+      // ctx.strokeStyle = "red";
+      // ctx.moveTo(5, 5);
+      // ctx.lineTo(5, 140);
+      // ctx.stroke();
+
+      // ctx.lineWidth = 2;
+      // ctx.strokeStyle = "yellow";
+      // // ctx.beginPath();
+      // ctx.moveTo(19, 5);
+      // ctx.lineTo(19, 140);
+      // ctx.stroke();
+
+      // ctx.lineWidth = 2;
+      // ctx.strokeStyle = "yellow";
+      // // ctx.beginPath();
+      // ctx.moveTo(19, 5);
+      // ctx.lineTo(19, 140);
+      // ctx.stroke();
+    }
+    function drawRect() {
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = 'red'
+      ctx.strokeRect(50,50, 100,100)
+      ctx.lineWidth = 10;
+      ctx.strokeStyle = 'green'
+      ctx.strokeRect(200, 200, 100,100)
+    }
+    draw();
+    drawRect();
+  }, []);
+  return (
+    <div>
+      <canvas
+        style={{ border: "1px solid black" }}
+        ref={canvasRef}
+        width="400"
+        height="400"
+        id="tutorial"
+      ></canvas>
+    </div>
+  );
 }
 
 export default Style;
