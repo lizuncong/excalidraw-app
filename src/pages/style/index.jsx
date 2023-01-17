183de77377e2cf6229548c5ea281067652db44cc
lizuncong <2900170896@qq.com>
2023-01-17 23:54:21 +0800

    feat: 路径绘制

--- src/pages/style/index.jsx
+++ src/pages/style/index.jsx
@@ -7,33 +7,31 @@ function Style() {
     const ctx = canvas.getContext("2d");
 
     function draw() {
-      // ctx.lineWidth = 1;
-      // ctx.strokeStyle = "red";
-      // ctx.moveTo(5, 5);
-      // ctx.lineTo(5, 140);
-      // ctx.stroke();
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "red";
+      ctx.moveTo(5, 5);
+      ctx.lineTo(5, 140);
+      ctx.stroke();
 
-      // ctx.lineWidth = 2;
-      // ctx.strokeStyle = "yellow";
-      // // ctx.beginPath();
-      // ctx.moveTo(19, 5);
-      // ctx.lineTo(19, 140);
-      // ctx.stroke();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      ctx.moveTo(19, 5);
+      ctx.lineTo(19, 140);
+      ctx.stroke();
 
-      // ctx.lineWidth = 2;
-      // ctx.strokeStyle = "yellow";
-      // // ctx.beginPath();
-      // ctx.moveTo(19, 5);
-      // ctx.lineTo(19, 140);
-      // ctx.stroke();
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = "blue";
+      ctx.moveTo(33, 5);
+      ctx.lineTo(33, 140);
+      ctx.stroke();
     }
     function drawRect() {
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = 'red'
-      ctx.strokeRect(50,50, 100,100)
-      ctx.lineWidth = 10;
-      ctx.strokeStyle = 'green'
-      ctx.strokeRect(200, 200, 100,100)
+      // ctx.lineWidth = 1;
+      // ctx.strokeStyle = 'red'
+      // ctx.strokeRect(50,50, 100,100)
+      // ctx.lineWidth = 10;
+      // ctx.strokeStyle = 'green'
+      // ctx.strokeRect(200, 200, 100,100)
     }
     draw();
     drawRect();
