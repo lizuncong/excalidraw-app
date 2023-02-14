24a143c7d0c6b98b93eb73ade41be2b0192f6564
lizuncong <2900170896@qq.com>
2023-02-14 23:49:17 +0800

    feat: 如何判断是否超出绘制限制

--- src/pages/max-size/index.jsx
+++ src/pages/max-size/index.jsx
@@ -5,6 +5,32 @@ import "./index.less";
 
 function Index() {
   const canvasRef = useRef(null);
+  const isCanvasExceedsMaximumSize = (width, height) => {
+    const testCvs = document.createElement("canvas");
+    testCvs.width = width;
+    testCvs.height = height;
+    const testCtx = testCvs.getContext("2d");
+    testCtx.fillRect(width - 1, height - 1, 1, 1);
+
+    const cropCvs = document.createElement("canvas");
+    cropCvs.width = 1;
+    cropCvs.height = 1;
+    const cropCtx = cropCvs.getContext("2d");
+    cropCtx.drawImage(testCvs, width - 1, height - 1, 1, 1, 0, 0, 1, 1);
+
+    const isTestPass =
+      cropCtx && cropCtx.getImageData(0, 0, 1, 1).data[3] !== 0;
+
+    if (isTestPass) {
+      console.log(`canvas绘制正常，宽度：${width}，高度：${height}`);
+    } else {
+      console.error(
+        `canvas绘制超出最大尺寸限制，宽度：${width}，高度：${height}`
+      );
+    }
+    return isTestPass;
+  };
+
   useEffect(() => {
     try {
       const canvas = canvasRef.current;
@@ -14,7 +40,7 @@ function Index() {
       const ctx = canvas.getContext("2d");
       ctx.fillStyle = "red";
       ctx.fillRect(0, 0, 300, 300);
-      console.log(canvas.toDataURL());
+      isCanvasExceedsMaximumSize(canvas.width, canvas.height);
     } catch (e) {
       console.log("error===", e);
     }
