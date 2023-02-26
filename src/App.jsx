e03931a7d855f21d4b434dea4e9a997308131171
lizuncong <2900170896@qq.com>
2023-02-26 13:10:21 +0800

    fix: 静态资源路径

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
 import MENUS from "./menus";
 import "./App.css";
 
-window.EXCALIDRAW_ASSET_PATH = "/";
+console.log('process.evne', process.env)
+window.EXCALIDRAW_ASSET_PATH = process.env.NODE_ENV === "development" ? "/" : '/excalidraw-app/';
 
 
 function App() {
