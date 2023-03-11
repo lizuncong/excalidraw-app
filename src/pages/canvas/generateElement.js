c96954a1f209791439d4fbcc572d619d4036e7b8
lizuncong <2900170896@qq.com>
2023-03-11 13:37:36 +0800

    feat: 生成元素

--- src/pages/canvas/generateElement.js
+++ src/pages/canvas/generateElement.js
@@ -1,4 +1,5 @@
-import { createElement } from "./element/newElement";
+import { createElement, newTextElement } from "./element/newElement";
+import { rgb } from "@/util";
 
 const getRandomValue = (n, m) => {
   return Number((Math.random() * (m - n) + n).toFixed(5));
@@ -8,28 +9,14 @@ const generateElements = (count, type, appState) => {
   console.log("generate elements...", count, type);
   let element;
   for (let i = 0; i < count; i++) {
-    const x = getRandomValue(
+    let x = getRandomValue(
       -appState.scrollX,
       appState.canvasWidth - appState.scrollX
     );
-    const y = getRandomValue(
+    let y = getRandomValue(
       -appState.scrollY,
       appState.canvasHeight - appState.scrollY
     );
-    const width =
-      Math.abs(
-        getRandomValue(
-          -appState.scrollX - x,
-          appState.canvasWidth - appState.scrollX - x
-        )
-      ) + 2;
-    const height =
-      Math.abs(
-        getRandomValue(
-          -appState.scrollY - y,
-          appState.canvasHeight - appState.scrollY - y
-        )
-      ) + 2;
     element = createElement({
       elementType: type,
       pointerDownState: {
@@ -40,8 +27,50 @@ const generateElements = (count, type, appState) => {
       },
       appState,
     });
-    element.width = width;
-    element.height = height;
+    if (type === "rectangle") {
+      const width =
+        Math.abs(
+          getRandomValue(
+            -appState.scrollX - x,
+            appState.canvasWidth - appState.scrollX - x
+          )
+        ) + 2;
+      const height =
+        Math.abs(
+          getRandomValue(
+            -appState.scrollY - y,
+            appState.canvasHeight - appState.scrollY - y
+          )
+        ) + 2;
+      element.width = width;
+      element.height = height;
+    } else if (type === "freedraw") {
+      const pointsNum = getRandomValue(20, 100);
+      for (let i = 0; i < pointsNum; i++) {
+        x = getRandomValue(x, x + 10);
+        y = getRandomValue(y, y + 10);
+        element.points.push([x, y]);
+      }
+    } else if (type === "text") {
+      const text = `${x}, ${y}`;
+      element = newTextElement({
+        x,
+        y,
+        strokeColor: rgb(),
+        backgroundColor: "transparent",
+        fillStyle: "hachure",
+        strokeWidth: 1,
+        strokeStyle: "solid",
+        text,
+        width: text.length * 20,
+        height: 25,
+        fontSize: 20,
+        fontFamily: "Virgil", // Helvetica Cascadia
+        textAlign: "left",
+        verticalAlign: "top",
+      });
+    }
+
     elements.push(element);
   }
 
