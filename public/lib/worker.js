6982413b108b5d7a2ba4b7ae79993c93dcff03c8
lizuncong <2900170896@qq.com>
2023-03-15 22:30:44 +0800

    feat: stash

--- public/lib/worker.js
+++ public/lib/worker.js
@@ -1,24 +1,79 @@
 let canvas = null;
 let ctxWorker = null;
+let canvasWidth;
+let canvasHeight;
+let elements = [];
+let count = 15000;
 self.onmessage = (event) => {
-  const { canvasWorker, scale } = event.data;
-  canvas = canvasWorker;
-  ctxWorker = canvas.getContext("2d");
-  ctxWorker.scale(scale, scale);
-  startCounting();
+  const { canvasWorker, type, scale } = event.data;
+  elements = generateElements(count);
+  if (type === "redraw") {
+    startCounting();
+  } else {
+    canvas = canvasWorker;
+    ctxWorker = canvas.getContext("2d");
+    ctxWorker.scale(scale, scale);
+    canvasWidth = canvas.width;
+    canvasHeight = canvas.height;
+    startCounting();
+  }
 };
 
-let counter = 0;
 function startCounting() {
-  setInterval(() => {
-    redrawCanvasB();
-    counter++;
-  }, 100);
+  // setInterval(() => {
+  elements = generateElements(count);
+  redrawCanvasB();
+  // }, 100);
 }
 
 function redrawCanvasB() {
+  console.log("redraw...", elements.length);
   ctxWorker.clearRect(0, 0, canvas.width, canvas.height);
-  ctxWorker.font = "24px Verdana";
-  ctxWorker.textAlign = "center";
-  ctxWorker.fillText(counter, canvas.width / 2, canvas.height / 2);
+  ctxWorker.save();
+
+  elements.forEach((ele) => {
+    ctxWorker.save();
+
+    ctxWorker.lineJoin = "round";
+    ctxWorker.lineCap = "round";
+    ctxWorker.lineWidth = ele.strokeWidth;
+    ctxWorker.strokeStyle = ele.strokeStyle;
+    ctxWorker.strokeRect(0, 0, ele.width, ele.height);
+
+    ctxWorker.restore();
+  });
+
+  ctxWorker.restore();
 }
+
+const rgb = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+};
+const getRandomValue = (n, m) => {
+  return Number((Math.random() * (m - n) + n).toFixed(5));
+};
+
+const generateElements = (count) => {
+  const elements = [];
+  let element;
+  for (let i = 0; i < count; i++) {
+    let x = getRandomValue(0, canvasWidth);
+    let y = getRandomValue(0, canvasHeight);
+    element = {
+      x,
+      y,
+      strokeStyle: rgb(),
+    };
+    const width = Math.abs(getRandomValue(-x, canvasWidth - x)) + 2;
+    const height = Math.abs(getRandomValue(-y, canvasHeight - y)) + 2;
+    element.width = width;
+    element.height = height;
+
+    elements.push(element);
+  }
+
+  return elements;
+};
