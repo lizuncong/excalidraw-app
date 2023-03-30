895e02dd158656e0d90f300af1b03bfd349a9d70
李尊聪 <lizuncong@cvte.com>
2023-03-30 18:03:07 +0800

    feat: stash

--- src/pages/dom-render/base/renderer/svg/renderElement.js
+++ src/pages/dom-render/base/renderer/svg/renderElement.js
@@ -1,33 +1,35 @@
+import { getElementAbsoluteCoords, distance } from "@/util";
+
 export const deleteElementCache = () => {};
 
 export const clearElementCache = () => {};
 
 const TO_FIXED_PRECISION = /(\s?[A-Z]?,?-?[0-9]*\.[0-9]{0,2})(([0-9]|e|-)*)/g;
-function med(A, B) {
-  return [(A[0] + B[0]) / 2, (A[1] + B[1]) / 2];
-}
-const getSvgPathFromStroke = (points) => {
-  if (!points.length) {
-    return "";
-  }
+// function med(A, B) {
+//   return [(A[0] + B[0]) / 2, (A[1] + B[1]) / 2];
+// }
+// const getSvgPathFromStroke = (points) => {
+//   if (!points.length) {
+//     return "";
+//   }
 
-  const max = points.length - 1;
+//   const max = points.length - 1;
 
-  return points
-    .reduce(
-      (acc, point, i, arr) => {
-        if (i === max) {
-          acc.push(point, med(point, arr[0]), "L", arr[0], "Z");
-        } else {
-          acc.push(point, med(point, arr[i + 1]));
-        }
-        return acc;
-      },
-      ["M", points[0], "Q"]
-    )
-    .join(" ")
-    .replace(TO_FIXED_PRECISION, "$1");
-};
+//   return points
+//     .reduce(
+//       (acc, point, i, arr) => {
+//         if (i === max) {
+//           acc.push(point, med(point, arr[0]), "L", arr[0], "Z");
+//         } else {
+//           acc.push(point, med(point, arr[i + 1]));
+//         }
+//         return acc;
+//       },
+//       ["M", points[0], "Q"]
+//     )
+//     .join(" ")
+//     .replace(TO_FIXED_PRECISION, "$1");
+// };
 export const renderElementToSvg = (element, renderConfig, appState) => {
   let el;
   switch (element.type) {
@@ -42,7 +44,7 @@ export const renderElementToSvg = (element, renderConfig, appState) => {
             position: "absolute",
             left: `${element.x}px`,
             top: `${element.y}px`,
-            background: "",
+            // background: "grey",
           }}
           xmlns="http://www.w3.org/2000/svg"
           version="1.1"
@@ -61,39 +63,36 @@ export const renderElementToSvg = (element, renderConfig, appState) => {
       break;
     }
     case "freedraw": {
-      console.log("points===", element);
-      const points = element.points
-        .map((p) => [p[0] - element.x, p[1] - element.y]);
+      let [x1, y1, x2, y2] = getElementAbsoluteCoords(element);
+      const width = distance(x1, x2);
+      const height = distance(y1, y2);
+      const points = element.points.map((p) => [
+        p[0] - x1,
+        p[1] - y1,
+      ]);
       el = (
         <svg
           id={element.id}
           key={element.id}
-          //   width={element.width}
-          //   height={element.height}
+          width={width}
+          height={height}
           style={{
             position: "absolute",
-            left: `${element.x}px`,
-            top: `${element.y}px`,
-            background: "grey",
+            left: `${x1}px`,
+            top: `${y1}px`,
+            // background: "grey",
           }}
           xmlns="http://www.w3.org/2000/svg"
           version="1.1"
         >
-          <path
-            d={getSvgPathFromStroke(points)}
-            stroke="blue"
-            stroke-width="5"
-            fill="none"
-          />
-
-          {/* <polyline
-            points={points}
+          <polyline
+            points={points.join(" ")}
             style={{
               fill: "none",
               strokeWidth: element.strokeWidth,
               stroke: element.strokeStyle,
             }}
-          /> */}
+          />
         </svg>
       );
       break;
